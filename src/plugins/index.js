89e07fc9995821567ba586d36a6fb0550e94b540
ArShown <arshown@megalive.com.tw>
2020-10-22 09:47:22 +0800

    chore: plugins 載入自動化

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,8 +1,21 @@
-import axios from "./axios";
+const path = require("path");
+const files = require.context(".", true, /\.js$/);
+const modules = [];
+
+files.keys().forEach((key) => {
+  if (key === "./index.js") return;
+  const { alias, value } = require(`@/plugins/${path.normalize(key)}`).default;
+  modules.push({
+    alias,
+    value,
+  });
+});
 
 const layouts = {
   install: (app) => {
-    app.config.globalProperties[axios.alias] = axios.value;
+    modules.forEach((m) => {
+      app.config.globalProperties[m.alias] = m.value;
+    });
   },
 };
 
